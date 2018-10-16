216c33709a76c53ae30c6dbee975c7393a0c883c
Szymon <szyhan@gmail.com>
2018-10-16 12:50:57 +0200

    add essentials to gitignore

--- app/src/main/java/pl/szymonhanzel/alarmeclient/component/SplashScreenActivity.java
+++ app/src/main/java/pl/szymonhanzel/alarmeclient/component/SplashScreenActivity.java
@@ -1,35 +1,18 @@
 package pl.szymonhanzel.alarmeclient.component;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 
 import pl.szymonhanzel.alarmeclient.MainActivity;
 
-public class SplashScreenActivity extends Activity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    private class ActivityStarter extends Thread {
-        private static final int CZAS = 2000;
-
-        @Override
-        public void run() {
-            try {
-                // tutaj wrzucamy wszystkie akcje potrzebne podczas ładowania aplikacji
-                Thread.sleep(CZAS);
-            } catch (Exception e) {
-                Log.e("SplashScreen", e.getMessage());
-            }
-
-            // Włącz główną aktywność
-            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-            SplashScreenActivity.this.startActivity(intent);
-            SplashScreenActivity.this.finish();
-        }
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
