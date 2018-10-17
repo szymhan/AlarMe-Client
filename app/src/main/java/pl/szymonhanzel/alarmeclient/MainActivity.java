712fbc228b4da131d5849d1880bf646ae2529a99
Szymon <szyhan@gmail.com>
2018-10-17 23:47:06 +0200

    added potentially useful GPS tracker, obligatory manifest permissions and Firebase Value Listener to MainActivity.

--- app/src/main/java/pl/szymonhanzel/alarmeclient/MainActivity.java
+++ app/src/main/java/pl/szymonhanzel/alarmeclient/MainActivity.java
@@ -8,13 +8,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import pl.szymonhanzel.alarmeclient.fragment.AlarMeFragment;
 import pl.szymonhanzel.alarmeclient.enumerator.NavigationEnum;
 import pl.szymonhanzel.alarmeclient.fragment.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("alarms");
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -29,10 +38,21 @@ public class MainActivity extends AppCompatActivity {
                     initFragment(NavigationEnum.SETTINGS);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
+        }
+    };
+
+    private ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
         }
     };
 
@@ -41,11 +61,20 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initNavigationBar();
+        myRef.addValueEventListener(valueEventListener);
+    }
+
+
+    private void initDatabaseListener() {
+
+    }
+
+    private void initNavigationBar() {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         initFragment(NavigationEnum.HOME);
     }
-
 
     private void initFragment(NavigationEnum ne) {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
